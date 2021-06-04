903bdb611227da9392f8c28401dc33c5047b3506
Stephen Geller <stephen.gb.geller+git@gmail.com>
2021-06-04 16:32:00 +0100

    Begin migrating Prism to EC2 pattern

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -3,10 +3,17 @@ import "source-map-support/register";
 import { App } from "@aws-cdk/core";
 import { PrismStack } from "../lib/prism";
 import { PrismAccess } from "../lib/prism-access";
+import { PrismGuCdkStack } from "../lib/prism-gucdk";
 
 const app = new App();
 
 new PrismStack(app, "Prism", {
+  description: "Prism - service discovery",
+  migratedFromCloudFormation: true,
+  stack: "deploy",
+});
+
+new PrismGuCdkStack(app, "Prism", {
   description: "Prism - service discovery",
   migratedFromCloudFormation: true,
   stack: "deploy",
